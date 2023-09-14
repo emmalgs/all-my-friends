91a1c110d6ab7b46f03d0ebc1b82f53bedb2e251
Emma Gerigscott <elgerig@gmail.com>
2023-09-14 14:27:09 -0700

    add form to modal body

--- src/components/screens/inviteFriendsModal.jsx
+++ src/components/screens/inviteFriendsModal.jsx
@@ -3,17 +3,32 @@ import React from "react";
 function FriendsModal(props) {
   return (
     <div>
-      <div onClick={props.exitClick}>x</div>
-      <h1>Invite Friends</h1>
-      {props.friendsList.map((friend, index) => {
-        return (
-          <div key={index}>
-            <div>{friend.firstName} {friend.lastName}</div>
-            <div>{friend.email}</div>
-          </div>
-        );
-      })}
-      <button>Invite Friends</button>
+      <div className="modal-header">
+        <span onClick={props.exitClick}>x</span>
+        <h1>Invite Friends</h1>
+      </div>
+      <div className="modal-body">
+        <form>
+          {props.friendsList.map((friend, index) => {
+            return (
+              <div key={index} className="friend-select">
+                <label className="info">
+                  <div>
+                    {friend.firstName} {friend.lastName}
+                  </div>
+                  <div>{friend.email}</div>
+                </label>
+                <input
+                  type="checkbox"
+                  name={friend.email}
+                  value={friend.email}
+                />
+              </div>
+            );
+          })}
+          <button>Invite Friends</button>
+        </form>
+      </div>
     </div>
   );
 }
