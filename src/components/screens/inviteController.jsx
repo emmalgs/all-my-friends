4b423123a6c5d601cb0dd3a20f7c38fdf21cf2c7
Emma Gerigscott <elgerig@gmail.com>
2023-09-14 14:11:32 -0700

    useEffect to call api and get friends

--- src/components/screens/inviteController.jsx
+++ src/components/screens/inviteController.jsx
@@ -1,14 +1,30 @@
-import React, { Component } from "react";
-import { Home } from "./home";
+import React, { useState, useEffect } from "react";
+import Home from "./home";
+import FriendsModal from "./inviteFriendsModal";
+import { InviteApi } from "../../services/inviteApi";
 
-class InviteController extends Component {
-  render() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+function InviteController() {
+  const [showFriendsModal, setShowFriendsModal] = useState(false);
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const inviteApi = new InviteApi();
+    inviteApi.getCandidates().then((response) => {
+      setFriends(response);
+    });
+  }, []);
+
+  const toggleModal = () => {
+    setShowFriendsModal(!showFriendsModal);
+  };
+
+  let visibleScreen = null;
+  if (showFriendsModal) {
+    visibleScreen = <FriendsModal exitClick={toggleModal} friendsList={friends} />;
+  } else {
+    visibleScreen = <Home inviteClick={toggleModal} />;
   }
+  return <div>{visibleScreen}</div>;
 }
 
 export { InviteController };
