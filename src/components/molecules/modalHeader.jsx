16a2a8be7b74a9ef2006bfaf73bbcac6914ffb33
Emma Gerigscott <elgerig@gmail.com>
2023-09-15 11:43:35 -0700

    add inline cursor style for exit button

--- src/components/molecules/modalHeader.jsx
+++ src/components/molecules/modalHeader.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function ModalHeader(props) {
   return (
     <div className="modal-header">
-      <span onClick={props.exit}>&#215;</span>
       <h2>Invite Your 'Friends'</h2>
+      <span onClick={props.exit} style={{
+        cursor: "pointer",
+      }}>&#215;</span>
     </div>
   )
 }
