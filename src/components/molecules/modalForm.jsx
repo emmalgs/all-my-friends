1879da84467a79e355df3b17d375a58f643d8a08
Emma Gerigscott <elgerig@gmail.com>
2023-09-26 10:17:40 -0700

    add div for selection styling

--- src/components/molecules/modalForm.jsx
+++ src/components/molecules/modalForm.jsx
@@ -3,25 +3,29 @@ import Selection from "./selection";
 
 function ModalForm(props) {
   return (
-  <form onSubmit={props.handleSubmitClick} className="modal-form">
-    {props.list.map((item, index) => {
-      return (
-        <Selection
-          key={index}
-          class="friend-select"
-          value={item.email}
-          name={item.email}
-          selectionAction={(event) => {
-            props.handleItemSelection(event);
-          }}
-          labelClass="friend-select-label"
-          title={`${item.firstName} ${item.lastName}`}
-          subtitle={item.email}
-        />
-      );
-    })}
-    <button type="submit" disabled={props.btnDisableValue}>{props.buttonTextValue}</button>
-  </form>
+    <form onSubmit={props.handleSubmitClick} className="modal-form">
+      <div className="modal-selections">
+        {props.list.map((item, index) => {
+          return (
+            <Selection
+              key={index}
+              class="friend-select"
+              value={item.email}
+              name={item.email}
+              selectionAction={(event) => {
+                props.handleItemSelection(event);
+              }}
+              labelClass="friend-select-label"
+              title={`${item.firstName} ${item.lastName}`}
+              subtitle={item.email}
+            />
+          );
+        })}
+      </div>
+      <button type="submit" disabled={props.btnDisableValue}>
+        {props.buttonTextValue}
+      </button>
+    </form>
   );
 }
 
