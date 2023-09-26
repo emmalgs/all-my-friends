a1488f999cbd226147f34841f1bad3b30043e04e
Emma Gerigscott <elgerig@gmail.com>
2023-09-26 09:40:45 -0700

    refactor to use fetchMock to test inviteAPI service

--- src/__tests__/api/inviteApi.test.js
+++ src/__tests__/api/inviteApi.test.js
@@ -1,17 +1,13 @@
 import { InviteApi } from "../../services/inviteApi";
+import sampleData  from "../../components/data/inviteCandidates";
+import fetchMock from 'jest-fetch-mock';
 
-const { startMockServer, stopMockServer } = require("../../mock/mockServer");
+fetchMock.enableMocks();
 
 describe("inviteApi", () => {
-  beforeEach(() => {
-    startMockServer();
-  });
-  afterEach(() => {
-    stopMockServer();
-  });
-
   it("should get a list of candidates", async () => {
-    const api = new InviteApi("http://localhost:4000", "mock-auth-code");
+    fetchMock.mockResponseOnce(JSON.stringify(sampleData));
+    const api = new InviteApi("", "");
     const candidates = await api.getCandidates();
     expect(candidates[0]).toEqual({
       firstName: "Mary",
@@ -20,13 +16,16 @@ describe("inviteApi", () => {
     });
   });
 
-  // it('should return a message with the number of email invites sent', async () => {
-  //   const api = new InviteApi("http://localhost:4000", "mock-auth-code");
-  //   const emails = [
-  //     "greg@greg.com", 
-  //     "grog@grog.com",
-  //   ];
-  //   const response = await api.sendInvites(emails);
-  //   expect(response.message).toEqual("Invites sent to 2 emails");
-  // });
+  it('should return a message with the number of email invites sent', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({
+      message: "Invites sent to 2 emails"
+    }));
+    const api = new InviteApi("", "");
+    const emails = [
+      "greg@greg.com", 
+      "grog@grog.com",
+    ];
+    const response = await api.sendInvites(emails);
+    expect(response.message).toEqual("Invites sent to 2 emails");
+  });
 });
