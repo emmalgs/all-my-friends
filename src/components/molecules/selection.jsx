d914ee228d1636a15a63dd34cd2710e4a041f607
Emma Gerigscott <elgerig@gmail.com>
2023-09-26 11:43:33 -0700

    add selection styling

--- src/components/molecules/selection.jsx
+++ src/components/molecules/selection.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import  React, { useState } from "react";
 
 function Selection(props) {
+  const [selectionStyle, setSelectionStyle] = useState(disabled);
+
+  const disabled = {
+    border: "1px solid #a0a0a0",
+  };
+
+  const enabled = {
+    border: "1px solid #3b82f6",
+  };
+
+  const handleSelectionDivClick = (event) => {
+    event.target.checked
+      ? setSelectionStyle(enabled)
+      : setSelectionStyle(disabled);
+  };
+
   return (
-    <div className="selection-body">
+    <div
+      className="selection-body"
+      onChangeCapture={handleSelectionDivClick}
+      style={selectionStyle}
+    >
       <input
         type="checkbox"
         value={props.value}
