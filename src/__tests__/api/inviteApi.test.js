445c7583061d40cd3b2bb7c0350b9210bc0ff008
Emma Gerigscott <elgerig@gmail.com>
2023-09-26 09:50:56 -0700

    add error tests for bad request, bad format

--- src/__tests__/api/inviteApi.test.js
+++ src/__tests__/api/inviteApi.test.js
@@ -1,10 +1,10 @@
 import { InviteApi } from "../../services/inviteApi";
-import sampleData  from "../../components/data/inviteCandidates";
-import fetchMock from 'jest-fetch-mock';
+import sampleData from "../../components/data/inviteCandidates";
+import fetchMock from "jest-fetch-mock";
 
 fetchMock.enableMocks();
 
-describe("inviteApi", () => {
+describe("inviteApi.getCandidates", () => {
   it("should get a list of candidates", async () => {
     fetchMock.mockResponseOnce(JSON.stringify(sampleData));
     const api = new InviteApi("", "");
@@ -16,25 +16,50 @@ describe("inviteApi", () => {
     });
   });
 
-  it('should return an error message if the auth code is invalid', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify({
-      message: "Invalid auth code"
-    }));
+  it("should return an error message if the auth code is invalid", async () => {
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        message: "Invalid auth code",
+      })
+    );
     const api = new InviteApi("", "");
     const response = await api.getCandidates();
     expect(response.message).toEqual("Invalid auth code");
   });
+});
 
-  it('should return a message with the number of email invites sent', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify({
-      message: "Invites sent to 2 emails"
-    }));
+describe("inviteApi.sendInvites", () => {
+  it("should return a message with the number of email invites sent", async () => {
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        message: "Invites sent to 2 emails",
+      })
+    );
     const api = new InviteApi("", "");
-    const emails = [
-      "greg@greg.com", 
-      "grog@grog.com",
-    ];
+    const emails = ["greg@greg.com", "grog@grog.com"];
     const response = await api.sendInvites(emails);
     expect(response.message).toEqual("Invites sent to 2 emails");
+  });
+  it("should return an error message if the email format is invalid", async () => {
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        message: "Invalid email format",
+      })
+    );
+    const api = new InviteApi("", "");
+    const emails = ["hank"];
+    const response = await api.sendInvites(emails);
+    expect(response.message).toEqual("Invalid email format");
+  });
+  it('should return a 400 bad request if the array is empty', async () => {
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        message: "empty array",
+      })
+    );
+    const api = new InviteApi("", "");
+    const emails = [];
+    const response = await api.sendInvites(emails);
+    expect(response.message).toEqual("empty array");
   });
 });
